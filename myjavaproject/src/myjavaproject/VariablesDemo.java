79dcc9568f07a016ce1c9683ac35f503886bef0f
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-02-05 22:38:58 +0530

    fourth commit

---
+++ myjavaproject/src/myjavaproject/VariablesDemo.java
@@ -0,0 +1,24 @@
+package myjavaproject;
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          
+		
+		// Declare a variable and intialise
+		int a=129;
+		//-128 and a maximum value of 127
+		System.out.println(a);
+		
+		float b = 2.5f;
+		System.out.println(b);
+		
+		char ch='A';
+		System.out.println(ch);
+		
+		boolean result= true;
+		System.out.println(result);
+	}
+
+}
