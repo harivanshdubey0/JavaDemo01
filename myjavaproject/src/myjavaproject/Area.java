1e23fb44bafae54122a6ff69d693629745000d77
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-02-05 22:00:08 +0530

    first program commit

---
+++ myjavaproject/src/myjavaproject/Area.java
@@ -0,0 +1,14 @@
+package myjavaproject;
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float radius= 6.5f;
+		final float Pie=3.14f;
+		double area =Pie * radius * radius;
+		System.out.print("Area of circle is :" +area);
+
+	}
+
+}
