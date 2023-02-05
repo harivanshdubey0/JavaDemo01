72dd8e66ce289f474df0251ae23ce04c06e628b4
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-02-05 22:37:00 +0530

    third commit

---
+++ myjavaproject/src/myjavaproject/IfDemo.java
@@ -0,0 +1,23 @@
+package myjavaproject;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    // Person > 45 old , other wise young
+		
+		int age =29;
+		if (age>45)
+		{
+		  System.out.println("old");
+		  
+		}
+		else
+		{
+		 System.out.println("Young"); 
+		 
+		}
+
+	}
+
+}
