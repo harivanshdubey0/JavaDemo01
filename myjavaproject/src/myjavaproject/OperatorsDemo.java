31c1fa372b70509e7c6e5c5907285e56717f3ea7
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-02-06 23:51:30 +0530

    commit operatorsDemo

---
+++ myjavaproject/src/myjavaproject/OperatorsDemo.java
@@ -0,0 +1,19 @@
+package myjavaproject;
+
+public class OperatorsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		  + operator in java has 2 different behaviours
+		  
+		  It is used for addition of numeric values
+		  It is used for contatination of strings
+		  
+		 */
+       
+		System.out.println(10+20);
+	}
+
+}
