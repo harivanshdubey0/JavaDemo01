92f80f7caccd561c16aa6586145e81d21f60e2c2
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-02-17 23:12:04 +0530

    Commit to Registration program

---
+++ myjavaproject/src/myjavaproject/Registration.java
@@ -0,0 +1,35 @@
+package myjavaproject;
+
+public class Registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  /*student type
+        *
+        * Student name
+        * college
+        * email d
+        * phone number
+        * brach
+        * year of passpot
+        * backlog
+        *
+        */
+       String name = "Priyal";
+       String college= "Simpli";
+       String emaild = "abc@doe.com";
+       long phone_number =  33254887L;
+       Short Year_of_passout = 2005;
+       Boolean has_backlog = false;
+       System.out.println("Name of student " + name);
+       System.out.println("Name of college " +college);
+       System.out.println("Email ID " +emaild);
+       System.out.println("Phone number " +phone_number);
+       System.out.println("Year of passout " +Year_of_passout);
+       System.out.println("Has backlogs " +has_backlog);
+       System.out.println("Name of student " + name + "Name of college " +college);
+
+	}
+
+}
