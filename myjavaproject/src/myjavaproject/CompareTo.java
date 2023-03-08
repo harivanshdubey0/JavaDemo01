0a3c9419e0e90d375af49d096610e229dd235e8e
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-03-08 23:14:51 +0530

    commit to compare file in repo

---
+++ myjavaproject/src/myjavaproject/CompareTo.java
@@ -0,0 +1,18 @@
+package myjavaproject;
+
+public class CompareTo {
+
+	public static void main(String[] args) {
+	
+        String s1 = "Hello";
+        String s2= "Hello";
+        String s3= "Mella";
+        //char cl= 'H'
+        System.out.println("s1 compae s2 is :" + s1.compareTo(s2));
+        System.out.println("s1 compae s3 is :" + s1.compareTo(s3));
+       // System.out.println("unicode value fo H :" + Integer.toString(int(cl));
+        System.out.println(s1.concat(s3));
+
+	}
+
+}
