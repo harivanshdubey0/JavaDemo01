599674001e4a107001b5d569e8c82ae6468cac3d
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-03-08 22:51:43 +0530

    adding file for loop

---
+++ myjavaproject/src/myjavaproject/ForloopDemo.java
@@ -0,0 +1,22 @@
+package myjavaproject;
+
+public class ForloopDemo {
+
+	public static void main(String[] args) {
+		
+        /*For (initialisation, condition, iteration)
+        {
+           <<Statement>>
+        }*/
+        int num;
+        for (num = 10; num >0;num--)
+        {
+            System.out.println(("Weocomet to java programming :" + num));
+        }
+        System.out.println("Stament outside for loop");
+        //3, 5, 11, 13 prime numbers
+        //2,4,6, 12, Non prime number
+
+	}
+
+}
