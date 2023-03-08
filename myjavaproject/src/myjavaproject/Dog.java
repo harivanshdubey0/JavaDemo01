92a140847e3b1096efa9b5b3a108a697b13a6935
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-03-08 22:55:11 +0530

    commit dog file

---
+++ myjavaproject/src/myjavaproject/Dog.java
@@ -0,0 +1,32 @@
+package myjavaproject;
+
+public class Dog {
+
+    //state  data and instance variable
+
+    //behaviour is method
+    //it can hav both data + methods =  data memebers
+
+    int weight; // instance variable
+
+    // there are public, private, default, protected --- known modifiers, acccess specifier
+    // modifer, >> <return type >> < ,name> (parameter
+
+    //modiier is public this can be called rom other code
+    //return a valueit can rtuern. if void no value returen
+    //neme  set weight
+
+    //parameter is w
+
+
+// we are creating a method. a method will perform an operaton. it may return or not return an value
+    //if it is not returning a value then it is set as void
+    // setting the method as public means that this method is available for the call as well as other classes
+    //this method has a parameter
+    public void setweight(int w){
+        weight = w;
+    }
+    //this has issues
+	
+
+}
