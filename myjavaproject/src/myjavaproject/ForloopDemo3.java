2cc64f41b09254c7935d76ae121db425cefba225
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-03-08 22:54:01 +0530

    adding file forloop demo3

---
+++ myjavaproject/src/myjavaproject/ForloopDemo3.java
@@ -0,0 +1,17 @@
+package myjavaproject;
+
+public class ForloopDemo3 {
+
+	public static void main(String[] args) {
+		
+        int a,b;
+
+        for (a=1, b=4; a<b ;a++)
+        {
+            System.out.println("Value of a : "+ a);
+            System.out.println("Value of b : "+ b);
+        }
+
+	}
+
+}
