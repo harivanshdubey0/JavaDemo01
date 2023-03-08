5536ecc9f9ba3dcf1c5f93fa4a6c2f5eeb90b62f
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-03-08 22:56:53 +0530

    adding file Dog1

---
+++ myjavaproject/src/myjavaproject/Dog1.java
@@ -0,0 +1,20 @@
+package myjavaproject;
+
+public class Dog1 {
+
+	String breed;
+	int age;
+	String color;
+	
+	void barking() {
+		
+	}
+    void hungry() {
+    	
+    }
+    void sleeping() {
+    	
+    }
+    
+}
+
