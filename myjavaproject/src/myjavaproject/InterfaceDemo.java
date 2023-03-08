1c94ae576a387c95c714d905072491c0d4213ba2
Harivansh Dubey <harivanshdubey075@gmail.com>
2023-03-08 22:59:09 +0530

    adding file InterfaceDemo

---
+++ myjavaproject/src/myjavaproject/InterfaceDemo.java
@@ -0,0 +1,16 @@
+package myjavaproject;
+
+public class InterfaceDemo {
+
+	public void method01() {
+		System.out.println("implements of method 01 ");
+	}
+
+	public static void main(String[] args)
+
+	{
+		InterfaceDemo obj =new InterfaceDemo();
+		obj.method01();
+	}
+
+}
